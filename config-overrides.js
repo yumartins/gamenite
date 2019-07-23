26116a835f244889de2a79e95a54ed65fa9fb75e
yumartins <yuri@estudioflow.com.br>
2019-07-22 23:21:10 -0300

    base ok react

---
+++ config-overrides.js
@@ -0,0 +1,17 @@
+const { override, addBabelPlugins } = require('customize-cra');
+
+module.exports = override(
+
+    ...addBabelPlugins(
+        [
+            'babel-plugin-root-import',
+            {
+
+                'rootPathPrefix': '~',
+                'rootPathSuffix': 'src'
+
+            }
+        ]
+    )
+
+)
--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import './styles/base.css';
+import './styles/reset.css';
+
 const App = () => <h1>Hello World</h1>
 
 export default App;
